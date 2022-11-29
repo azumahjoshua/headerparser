d2bccd7fd48c4ec2823d15ae52a34b59640dae99
azumahjoshua <43423892+azumahjoshua@users.noreply.github.com>
2022-11-29 14:45:44 +0000

    add route for whoami

--- index.js
+++ index.js
@@ -20,10 +20,19 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
+// app.get('/api/hello', function (req, res) {
+//   res.json({ greeting: 'hello API' });
+// });
+// req.headers["accept-language"],
+app.get('/api/whoami',(req,res)=>{
+  // console.log(req.headers)
+  const responsesObject = {
+    "ipaddress" : req.ip,
+    "language" :  req.headers["accept-language"],
+    "software" : req.headers["user-agent"]
+  }
+  res.json(responsesObject)
+})
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
